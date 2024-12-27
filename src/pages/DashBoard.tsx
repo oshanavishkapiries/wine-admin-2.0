bb91137a5c406a98badc1112e0645f1154b60fb4
OSHAN AVISHKA <153918928+oshanavishkapiries@users.noreply.github.com>
2024-12-27 11:40:37 +0530

    bug-fixed-2

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,9 +1,10 @@
-
-
 const DashBoard = () => {
   return (
-    <div>DashBoard</div>
-  )
-}
+    <div className="h-screen flex flex-col items-center justify-center gap-4">
+      <div className="text-yellow-500 text-6xl">🚧</div>
+      <h1 className="text-2xl text-gray-600">Under Development</h1>
+    </div>
+  );
+};
 
 export default DashBoard
